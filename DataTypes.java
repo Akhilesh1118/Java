6bf33498e4a4da69a6f5ce30d225c97fe6015769
Akhilesh Yadav <akhileshyadav11189125198@gmail.com>
2025-09-14 21:10:12 +0530

    Added first Java program

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+    //             Data Types in Java
+            //            /          \
+            //          /              \  
+            //    Primitive || Size      \
+            //    1. byte        1 byte   Non- Primitive Data Types
+            //    2. short       2           1. String  
+            //    3. int         4            2. Arrays
+            //    4. long        8            3.Class 
+            //     5. float      4            4. Object    
+            //     6. double     8            5. Interface
+            //     7. char       2
+            //     8. boolean    1
+
+
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+    byte a = 10;
+    short b = 20;
+    int c = 30;
+    long d = 40L;
+    float e = 50.5f;
+    double f = 60.6;
+    char g = 'A';
+    boolean h = true;
+    System.out.println("Byte value: and range of byte is -128 to 127 ==" + a);
+    }
+}
+
---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main (String args[]) {
+        int a = 5 ; 
+        int b = 10 ;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Akhilesh Yadav ";
+        System.out.println(name);
+
+        a= b;
+        System.out.println(a);
+    }
+}
