4ccc0a6969986f0ad10cee81b21fc7ef660e7d20
Akhilesh Yadav <akhileshyadav11189125198@gmail.com>
2025-09-14 19:32:59 +0530

    first Change

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello { 
+   public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// Run this code using the command:
+//  javac Hello.java && java Hello.java 
+
