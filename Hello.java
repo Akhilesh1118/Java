0772b0e11ce68c476d250610d46c0509a2269a9e
Akhilesh Yadav <akhileshyadav11189125198@gmail.com>
2025-09-15 00:38:00 +0530

    Add java file

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello { 
-   public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-
-// Run this code using the command:
-//  javac Hello.java && java Hello.java 
-
