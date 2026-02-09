108c764d10b71a910d9cbfed931ce99d2465d727
Akhilesh Yadav <akhileshyadav11189125198@gmail.com>
2026-02-09 22:32:16 +0530

    Buy__SellStock.java

--- 8 Array/Buy__SellStock.java
+++ 8 Array/Buy__SellStock.java
@@ -1,24 +1,23 @@
 public class Buy__SellStock {
-    public static int buyAndSellStocks (int prices[]){
+    public static int buyAndSellStocks(int prices[]) {
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
-        for(int i=0; i<prices.length; i++){ // profit 
+        for (int i = 0; i < prices.length; i++) { // profit
             if (buyPrice < prices[i]) {
-                
-                int profit = prices[i] - buyPrice; // todays' profit 
+
+                int profit = prices[i] - buyPrice; // todays' profit
                 maxProfit = Math.max(maxProfit, profit);
-            }else{
+            } else {
                 buyPrice = prices[i];
             }
         }
-        return maxProfit; 
+        return maxProfit;
     }
-    
+
     public static void main(String[] args) {
-    int prices[] = {7,1,5,3,6,4} ;
-    System.out.println("Maximum profit is = "+ buyAndSellStocks(prices) ) ;
-    
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("Maximum profit is = " + buyAndSellStocks(prices));
+
     }
 }
-    
