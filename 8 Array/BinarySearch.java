213aadedf2b96cde1d2103c2b53916a7242bd8e1
Akhilesh Yadav <159260120+Akhilesh1118@users.noreply.github.com>
2026-03-16 21:57:30 +0530

    using for loop binarySearch

--- 8 Array/BinarySearch.java
+++ 8 Array/BinarySearch.java
@@ -17,9 +17,31 @@ public class BinarySearch {
         return -1;
     }
 
+    // Function using For loop 
+
+    public static boolean binarySearch(int[] array, int value)  {
+    int start = 0;
+    int end = array.length -1;              
+     
+    for (int i = 0; i < array.length; i++)   {
+        int middle = (end - start)/2;
+        if (array[i] == value)  {
+            return true;
+        }
+        else if (array[middle] > value)  {
+            end = middle - 1;
+        }
+        else    {
+            start = middle + 1;
+        }
+    }
+    return false;
+}
+    
     public static void main(String args[]) {
         int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
         int key = 10;
         System.out.println("Index for key is : " + binearySearch(numbers, key));
+        System.out.println(binarySearch(numbers, key ) );
     }
 }
